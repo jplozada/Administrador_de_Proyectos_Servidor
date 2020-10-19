66ffa261c9b3ddba50adf4ee7af3c6f26dcd9450
jplozada <juxnpx@gmail.com>
2020-10-19 18:23:32 -0400

    server created, port assingned

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('desde index.js');
+const express = require('express');
+
+// Crear el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Definir la página principal
+// app.get('/', (req, res) => {
+//     res.send('hola mundo')
+// })
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT }`)
+})
