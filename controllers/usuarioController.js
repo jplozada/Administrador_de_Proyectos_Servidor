37bc60bd2fe05abe8491803e228b5ff3a4728ee6
jplozada <juxnpx@gmail.com>
2020-10-20 17:33:03 -0400

    Jsonwebtoken for usuarios created

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,12 +1,13 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
 const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
     
     // Revisar si hay errores
     const errores = validationResult(req);
-    
+
     if(!errores.isEmpty()){
         return res.status(400).json({errores: errores.array()});
     }
@@ -30,11 +31,27 @@ exports.crearUsuario = async (req, res) => {
         const salt = await bcryptjs.genSalt(10);
         usuario.password = await bcryptjs.hash(password, salt);
 
+        // Crear y firmar el JWT
+        const payload = {
+            usuario:{
+                id: usuario.id
+            }
+        };
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmacion
+            res.json({ token });
+
+        });
+
         // Guardar usuario
         await usuario.save();
 
-        // Mensaje de confirmacion
-        res.json({ msg: 'Usuario creado correctamente' });
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
