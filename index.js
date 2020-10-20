ca0134d368d8b786e2d05b769aa65568b4b5361e
jplozada <juxnpx@gmail.com>
2020-10-20 15:42:43 -0400

    Usuarios rute and controller created

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 4000;
 //     res.send('hola mundo')
 // })
 
+//Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // Arrancar la app
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT }`)
