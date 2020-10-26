e1b0a08d56be38e2b77592bd2d65db46ee939309
jplozada <juxnpx@gmail.com>
2020-10-26 16:07:27 -0400

    Auth GET won't send password as response

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,10 +49,10 @@ exports.autenticarUsuario = async (req, res) => {
     }
 }
 
-// Obtiene quye usuario esta autenticado
+// Obtiene que usuario esta autenticado
 exports.usuarioAutenticado = async (req, res) => {
     try {
-        const usuario = await Usuario.findById(req.usuario.id);
+        const usuario = await Usuario.findById(req.usuario.id).select('-password');
         res.json({usuario});
     } catch (error) {
         console.log(error);
