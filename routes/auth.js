3b4607aca85eac55db933d72513a120d2668c253
jplozada <juxnpx@gmail.com>
2020-10-27 00:50:04 -0400

    Auth POST check validation email and password removed

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,10 @@ const auth = require('../middleware/auth');
 // api/auth
 router.post(
     '/', 
-    [
-        check('email', 'Agrega un email válido').isEmail(),
-        check('password', 'El password debe ser minimo de 6 caracteres').isLength({min: 6})
-    ], 
+    // [
+    //     check('email', 'Agrega un email válido').isEmail(),
+    //     check('password', 'El password debe ser minimo de 6 caracteres').isLength({min: 6})
+    // ], 
     authController.autenticarUsuario
 );
 
